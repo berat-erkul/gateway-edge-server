9d0374813964ea782f2181428577943a3293cd77
berat-erkul <beraterkul00@gmail.com>
2025-12-15 01:51:32 +0300

    -adds security related dependencies to our gateway

--- src/main/java/beraterkul/gatewayedgeserver/GatewayEdgeServerApplication.java
+++ src/main/java/beraterkul/gatewayedgeserver/GatewayEdgeServerApplication.java
@@ -2,6 +2,7 @@ package beraterkul.gatewayedgeserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
